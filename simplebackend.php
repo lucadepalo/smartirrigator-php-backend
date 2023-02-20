11bfaad7a51d65a398a6f8dda10c1eec2a63089d
Luca Depalo <31804822+lucadepalo@users.noreply.github.com>
2023-02-21 00:20:15 +0100

    Added server check and response
    check if primary key already exists
    response: regOK/regExist/regNot

--- simplebackend.php
+++ simplebackend.php
@@ -19,16 +19,37 @@
 		$pk =$_GET['pk'];
 		$type =$_GET['type'];
 		$ActType =$_GET['ActType'];
-		//query attuatori
+		
+		$sql_check = "SELECT COUNT(*) FROM `nodo_iot` WHERE `pk_nodo_iot` = '$pk';";
+		
+		$chk = mysqli_query($conn, $sql_check);
+
+		if ($chk && mysqli_num_rows($chk) > 0) {
+			$count = mysqli_fetch_row($chk)[0];
+		file_put_contents('debuggolo.txt', print_r(date('d/m/Y H:i:s', strtotime("+1 day"))."::count::".$count."\n", true),FILE_APPEND);
+		if ($count == 0) {
+			// If the primary key doesn't exist, insert it into the table
 		$sql_regact = "INSERT INTO `nodo_iot` (`pk_nodo_iot`, `nome`, `x`, `y`, `z`, `tipo`, `tipo_attuatore`, `tipo_sensore`, `valore_min`, `valore_max`, `statoCalcolato`, `p_min`, `p_max`, `fk_posto`, `fk_contenitore`, `icona`) VALUES ('$pk', NULL, NULL, NULL, NULL, '$type', '$ActType', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL) ON DUPLICATE KEY UPDATE pk_nodo_iot = '$pk';";
-		if ($conn->query($sql_regact) === TRUE) {
-			echo "regOK";
-			echo "Attuatore registrato con successo";
-		} 
-		else {
-			echo "regNOT";
-			echo "Error: " . $sql_regact . "<br>" . $conn->error;
+
+			$result = mysqli_query($conn, $sql_regact);
+
+			if ($result) {
+				echo "regOK";
+			} else {
+				echo "regNot";
+				echo "Error: " . mysqli_error($conn);
+			}
+		  } else {
+			// If the primary key already exists, echo a message
+			echo "regExist";
+		  }
+		} else {
+			echo "regNot";
+			echo "Error: " . mysqli_error($conn);
 		}
+		
+		
+		
     } elseif ($servicename == "GetActState") {
 		
 		$pk =$_GET['pk'];
@@ -55,14 +76,33 @@
 		$ValoreMinimo=$_GET['ValoreMinimo'];
 		$ValoreMassimo=$_GET['ValoreMassimo'];
 		
-		$sql_regsensor =
-		"INSERT INTO `nodo_iot` (`pk_nodo_iot`, `nome`, `x`, `y`, `z`, `tipo`, `tipo_attuatore`, `tipo_sensore`, `valore_min`, `valore_max`, `statoCalcolato`, `p_min`, `p_max`, `fk_posto`, `fk_contenitore`, `icona`) VALUES ('$pk', NULL, NULL, NULL, NULL, '$type', '$SensorType', NULL, '$ValoreMinimo', '$ValoreMassimo', NULL, NULL, NULL, NULL, NULL, NULL)	ON DUPLICATE KEY UPDATE pk_nodo_iot = '$pk';";
 		
-		if ($conn->query($sql_regsensor) === TRUE) {
-			echo "Sensore registrato con successo";
-		} 
-		else {
-			echo "Error: " . $sql_regsensor . "<br>" . $conn->error;
+		$sql_check = "SELECT COUNT(*) FROM `nodo_iot` WHERE `pk_nodo_iot` = '$pk';";
+		
+		$chk = mysqli_query($conn, $sql_check);
+
+		if ($chk && mysqli_num_rows($chk) > 0) {
+			$count = mysqli_fetch_row($chk)[0];
+		file_put_contents('debuggolo.txt', print_r(date('d/m/Y H:i:s', strtotime("+1 day"))."::count::".$count."\n", true),FILE_APPEND);
+		if ($count == 0) {
+			// If the primary key doesn't exist, insert it into the table
+			$sql_regsensor = "INSERT INTO `nodo_iot` (`pk_nodo_iot`, `nome`, `x`, `y`, `z`, `tipo`, `tipo_attuatore`, `tipo_sensore`, `valore_min`, `valore_max`, `statoCalcolato`, `p_min`, `p_max`, `fk_posto`, `fk_contenitore`, `icona`) VALUES ('$pk', NULL, NULL, NULL, NULL, '$type', '$SensorType', NULL, '$ValoreMinimo', '$ValoreMassimo', NULL, NULL, NULL, NULL, NULL, NULL)	ON DUPLICATE KEY UPDATE pk_nodo_iot = '$pk';";
+
+			$result = mysqli_query($conn, $sql_regsensor);
+
+			if ($result) {
+				echo "regOK";
+			} else {
+				echo "regNot";
+				echo "Error: " . mysqli_error($conn);
+			}
+		  } else {
+			// If the primary key already exists, echo a message
+			echo "regExist";
+		  }
+		} else {
+			echo "regNot";
+			echo "Error: " . mysqli_error($conn);
 		}
 	} elseif ($servicename == "RegData") {
 		$pk =$_GET['pk'];
